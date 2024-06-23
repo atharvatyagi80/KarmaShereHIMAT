2b2568c06239392d576ce82d06b848cdc07235e0
HimanshuChauhan05 <chauhanhimanshu260@gmail.com>
2024-06-23 13:52:05 +0530

    sky

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 document.addEventListener('DOMContentLoaded', () => {
     const likeIcons = document.querySelectorAll('.like-icon');
 
-    likeIcons.forEach(icon => {
+    likeIcons.forEach((icon) => {
         icon.addEventListener('click', () => {
-            icon.classList.toggle('liked');
+            if (icon.classList.contains('ri-thumb-up-line')) {
+                icon.classList.remove('ri-thumb-up-line');
+                icon.classList.add('ri-thumb-up-fill', 'liked');
+            } else {
+                icon.classList.remove('ri-thumb-up-fill', 'liked');
+                icon.classList.add('ri-thumb-up-line');
+            }
         });
     });
 });
